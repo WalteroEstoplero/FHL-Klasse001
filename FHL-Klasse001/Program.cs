10c9b3caa797a020225839510dd5cc9e8be97f99
Waltero Estoplero <waltser@web.de>
2019-10-08 19:01:45 +0200

    My first commit

---
+++ FHL-Klasse001/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FHL_Klasse001
+{
+    class Rennwagen
+    {
+        string name;
+        string farbe;
+        float leistung;
+        float hubraum;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
