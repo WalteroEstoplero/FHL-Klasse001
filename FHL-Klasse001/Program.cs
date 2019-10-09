7cdc3ee33b3c4af0721732c53cfad070cb927a91
Waltero Estoplero <waltser@web.de>
2019-10-09 18:06:02 +0200

    Weiter laut Script bis Überladen von Methoden

--- FHL-Klasse001/Program.cs
+++ FHL-Klasse001/Program.cs
@@ -6,6 +6,40 @@ using System.Threading.Tasks;
 
 namespace FHL_Klasse001
 {
+    class CPerson
+    {
+        string name;
+        int alter;
+
+        public void Ausgabe(int intParam)
+        {
+            alter = intParam;
+            Console.WriteLine(alter);
+        }
+
+        public void Ausgabe(string stringParam)
+        {
+            name = stringParam;
+            Console.WriteLine(name);
+        }
+
+        public void Ausgabe(int intParam, string stringParam)
+        {
+            alter = intParam;
+            name = stringParam;
+            Console.WriteLine(name + " " + alter);
+        }
+
+        public void Ausgabe(string stringParam, int intParam)
+        {
+            alter = intParam;
+            name = stringParam;
+            Console.WriteLine(name + " " + alter);
+        }
+
+    }
+
+
     class Rennwagen
     {
         string name;
@@ -13,6 +47,21 @@ namespace FHL_Klasse001
         string farbe;
         float leistung;
         float hubraum;
+
+        public Rennwagen()
+        {
+            besitzer = "Tim Buktu";
+        }
+
+        ~Rennwagen()
+        {
+            Console.WriteLine("Der Destruktor von Rennwagen wurde aufgerufen.");
+        }
+
+        public void SetFarbe(string farbe)
+        {
+            this.farbe = farbe;
+        }
 
         public void InitBesitzer()
         {
@@ -31,6 +80,18 @@ namespace FHL_Klasse001
 
     }
 
+    class CTest
+    {
+        public void Wertuebergabe(int param)
+        {
+            param = param + 10;
+        }
+
+        public void Referenzuebergabe(ref int param)
+        {
+            param = param + 10;
+        }
+    }
 
     class Program
     {
@@ -43,6 +104,27 @@ namespace FHL_Klasse001
 
             // Console.WriteLine(rennwagen.name);
             Console.WriteLine(rennwagen.GibBesitzer());
+
+            CTest einTest = new CTest();
+            int x = 5;
+            einTest.Wertuebergabe(x);
+            Console.WriteLine(x);
+
+            int y = 0;
+            einTest.Referenzuebergabe(ref y);
+            Console.WriteLine(y);
+
+            Rennwagen MyFlitzer = new Rennwagen();
+            Console.WriteLine("MyFlitzer " + MyFlitzer.GibBesitzer());
+
+
+            CPerson einePerson = new CPerson();
+            einePerson.Ausgabe("Max Becks");
+            einePerson.Ausgabe(34);
+            einePerson.Ausgabe("Robert Rappert", 56);
+            einePerson.Ausgabe(1, "Baby");
+
+
 
             Console.ReadKey();
         }
