b0a57107d3c4281bab37511fa62d7464664441cd
Waltero Estoplero <waltser@web.de>
2019-10-09 11:59:02 +0200

    Klasse lt. Script erweitert

--- FHL-Klasse001/Program.cs
+++ FHL-Klasse001/Program.cs
@@ -9,15 +9,42 @@ namespace FHL_Klasse001
     class Rennwagen
     {
         string name;
+        string besitzer;
         string farbe;
         float leistung;
         float hubraum;
+
+        public void InitBesitzer()
+        {
+            besitzer = "Flora Soft";
+        }
+
+        public void Name(string andererName)
+        {
+            besitzer = andererName;
+        }
+
+        public string GibBesitzer()
+        {
+            return besitzer;
+        }
+
     }
+
 
     class Program
     {
         static void Main(string[] args)
         {
+            Rennwagen rennwagen = new Rennwagen();
+            rennwagen.InitBesitzer();
+
+            rennwagen.Name("Herr Hermann Mann");
+
+            // Console.WriteLine(rennwagen.name);
+            Console.WriteLine(rennwagen.GibBesitzer());
+
+            Console.ReadKey();
         }
     }
 }
